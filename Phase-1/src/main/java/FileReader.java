c2133c0e81c4059c81d0bb6b7da7785877e586ab
mtndaghyani <matin.daghyani@gmail.com>
2021-07-24 13:17:54 +0430

    Implement FileReader

--- Phase-1/src/main/java/FileReader.java
+++ Phase-1/src/main/java/FileReader.java
@@ -1,5 +1,7 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class FileReader {
 
@@ -9,16 +11,20 @@ public class FileReader {
         this.directory = directory;
     }
 
-    public ArrayList<String> read(){
+    public ArrayList<String> read() throws FileNotFoundException {
         File folder = new File(this.directory);
         File[] listOfFiles = folder.listFiles();
         ArrayList<String> listOfContents = new ArrayList<String>();
         if (listOfFiles != null) {
             for (File file : listOfFiles) {
-                if (file.isFile()) {
-                    System.out.println(file.getName());
-                }
+                Scanner scanner = new Scanner(file);
+                scanner.useDelimiter("\\Z");
+                listOfContents.add(scanner.next());
             }
-        }
+        } else
+            System.err.println("Directory not found!");
+        return listOfContents;
+
     }
+
 }
