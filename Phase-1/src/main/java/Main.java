45f2ac206cfc0aa2bbab737f3e6f1dd0724bff08
mtndaghyani <matin.daghyani@gmail.com>
2021-07-25 10:47:45 +0430

    Implement main method

--- Phase-1/src/main/java/Main.java
+++ Phase-1/src/main/java/Main.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        InvertedIndex invertedIndex = new InvertedIndex("src/main/java/EnglishData");
+        String toSearch;
+        System.out.println("Enter a word:");
+        while (!(toSearch = scanner.nextLine()).equalsIgnoreCase("end")){
+            for (Integer id : invertedIndex.searchWord(toSearch)) {
+                System.out.println("doc" + id.toString());
+            }
+            System.out.println("Enter a word:");
+        }
+    }
 }
