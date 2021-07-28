6c232f515d4f8a3b192deaf814e09141107cc962
mtndaghyani <matin.daghyani@gmail.com>
2021-07-28 09:42:30 +0430

    Add Manager.java

--- Phase-1/src/main/java/Main.java
+++ Phase-1/src/main/java/Main.java
@@ -1,29 +1,8 @@
-import java.util.Scanner;
-
 public class Main {
     private final static String DATASET_PATH = "src/main/java/docs1";
-    private final static String END_DELIMITER = "$";
 
-    private static void run(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Indexing started...");
-        InvertedIndex invertedIndex = new InvertedIndex(DATASET_PATH);
-        SearchEngine engine = new SearchEngine(invertedIndex);
-        System.out.println("DONE");
-        String toSearch;
-        System.out.println("Enter something:");
-        while (!finished(toSearch = scanner.nextLine())) {
-            for (Integer id : engine.search(toSearch)) {
-                System.out.println("doc" + id.toString());
-            }
-            System.out.println("Enter something:");
-        }
-    }
 
-    private static boolean finished(String toSearch){
-        return toSearch.equalsIgnoreCase(END_DELIMITER);
-    }
     public static void main(String[] args) {
-        run();
+        new Manager(DATASET_PATH).run();
     }
 }
---
+++ Phase-1/src/main/java/Manager.java
@@ -0,0 +1,50 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Manager {
+    private final static String END_DELIMITER = "$";
+
+    private InvertedIndex invertedIndex;
+    private SearchEngine engine;
+
+    public Manager(String path) {
+        makeInvertedIndex(path);
+        makeSearchEngine();
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        String toSearch;
+        System.out.println("Enter something:");
+        while (!finished(toSearch = scanner.nextLine())) {
+            HashSet<Integer> docs = doSearch(toSearch);
+            printElements(docs);
+            System.out.println("Enter something:");
+        }
+    }
+
+    private void makeSearchEngine() {
+        engine = new SearchEngine(invertedIndex);
+    }
+
+    private void makeInvertedIndex(String path) {
+        System.out.println("Indexing started...");
+        invertedIndex = new InvertedIndex(path);
+        System.out.println("DONE");
+    }
+
+    private HashSet<Integer> doSearch(String toSearch) {
+        return engine.search(toSearch);
+    }
+
+    private void printElements(HashSet<Integer> elements) {
+        for (Integer id : elements) {
+            System.out.println("element" + id.toString());
+        }
+    }
+
+    private boolean finished(String toSearch) {
+        return toSearch.equalsIgnoreCase(END_DELIMITER);
+    }
+
+}
