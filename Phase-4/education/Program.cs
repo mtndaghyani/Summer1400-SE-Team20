c76eef2844e94f87ae0584460e0cc25876b31f34
mtndaghyani <matin.daghyani@gmail.com>
2021-08-02 12:08:29 +0430

    Initialize project

---
+++ Phase-4/education/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace education
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
