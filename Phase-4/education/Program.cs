7894b51955da3c9008fc115d025b94253a00dc4a
Soroush Taslimi <soroushtaslimi@yahoo.com>
2021-08-02 17:43:59 +0430

    read json file, GroupBy StudentNumber

--- Phase-4/education/Program.cs
+++ Phase-4/education/Program.cs
@@ -1,17 +1,45 @@
 ﻿using System;
 using System.IO;
 using System.Text.Json;
+using System.Collections;
+using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Linq;
 
 namespace education
 {
+    class ScoreSheet
+    {
+        public int StudentNumber{get; set;}
+        public string Lesson{get; set;}
+        public double Score{get; set;}
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             string content = File.ReadAllText("resources/Scores.json");
-            JObject json = JObject.Parse(content);
-            Console.WriteLine(json);
+            // List<Dictionary<string, string>> Scores = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(content);
+            List<ScoreSheet> Scores = JsonConvert.DeserializeObject<List<ScoreSheet>>(content);
+            foreach(var x in Scores){
+                Console.WriteLine(x.Lesson);
+            }
+
+            var MyList = Scores.GroupBy(x => x.StudentNumber,
+                            x => x.Score,
+                            (key, val) => new {StudentNumber=key, Average=val.ToList().Average()}).ToList();
+
+            foreach(var x in MyList){
+                Console.WriteLine(x);
+            }
+            /*
+            foreach(var dictionary in Scores){
+                var asString = string.Join(Environment.NewLine, dictionary);
+                Console.WriteLine(asString);
+            }
+            */
         }
     }
 }
