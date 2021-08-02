d435f1f5843a2bbb8b46240b9620f77d43c1f5f2
mtndaghyani <matin.daghyani@gmail.com>
2021-08-02 10:57:46 +0430

    Make path cross platform

--- Phase-1/src/test/java/FileReaderTests.java
+++ Phase-1/src/test/java/FileReaderTests.java
@@ -1,13 +1,17 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class FileReaderTests {
 
     @Test
     public void testReadFilesWithCorrectPath(){
-        FileReader fileReader = new FileReader("src/test/testDocs");
+        Path currentPath = Paths.get(System.getProperty("user.dir"));
+        Path filePath = Paths.get(currentPath.toString(), "src", "test", "testDocs");
+        FileReader fileReader = new FileReader(filePath.toString());
         Assertions.assertEquals(getContent(), fileReader.read());
     }
 
