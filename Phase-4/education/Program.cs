62680542479bbe79a080785e0a0f07b0aa9800e9
mtndaghyani <matin.daghyani@gmail.com>
2021-08-04 09:59:11 +0430

    Refactor the project

--- Phase-4/education/Program.cs
+++ Phase-4/education/Program.cs
@@ -1,56 +1,38 @@
 ﻿using System;
 using System.IO;
-using System.Text.Json;
-using System.Collections;
 using System.Collections.Generic;
-using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using System.Linq;
 
 namespace education
 {
-    
-    class ScoreSheet
-    {
-        public int StudentNumber{get; set;}
-        public string Lesson{get; set;}
-        public double Score{get; set;}
-    }
-
-    class Student
-    {
-        public int StudentNumber { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-    }
-
     class Program
     {
         private const string SlnPath = "../../../";
         private const string ScorePath = SlnPath + "resources/Scores.json";
         private const string StudentsPath = SlnPath + "resources/Students.json";
-        private const int NumTopStudents = 3;
+        private const int TopStudentsCount = 3;
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine(System.IO.Directory.GetCurrentDirectory());
-            string scoresContent = File.ReadAllText(ScorePath);
-            List<ScoreSheet> scores = JsonConvert.DeserializeObject<List<ScoreSheet>>(scoresContent);
-            string studentsContent = File.ReadAllText(StudentsPath);
-            List<Student> students = JsonConvert.DeserializeObject<List<Student>>(studentsContent);
+            var scoresContent = File.ReadAllText(ScorePath);
+            var scores = JsonConvert.DeserializeObject<List<ScoreSheet>>(scoresContent);
+            var studentsContent = File.ReadAllText(StudentsPath);
+            var students = JsonConvert.DeserializeObject<List<Student>>(studentsContent);
 
-            var averageList = scores.GroupBy(x => x.StudentNumber,
-                    x => x.Score,
-                    (key, val) => 
-                        new {StudentNumber=key, Average=val.ToList().Average()}).ToList();
+            var averageList = scores.GroupBy(
+                x => x.StudentNumber,
+                x => x.Score,
+                (key, val) => new {StudentNumber = key, Average = val.ToList().Average()}).ToList();
 
-            var topStudents = averageList.Join(students,
+            var topStudents = averageList.Join(
+                    students,
                     x => x.StudentNumber, y => y.StudentNumber,
-                    (q, w) =>
-                    new {Average=q.Average, FirstName=w.FirstName, LastName=w.LastName})
-                    .OrderByDescending(q => q.Average).ToList();
+                    (q, w) => new {Average = q.Average, FirstName = w.FirstName, LastName = w.LastName})
+                .OrderByDescending(q => q.Average)
+                .ToList();
 
-            topStudents = topStudents.Take(NumTopStudents).ToList();
+            topStudents = topStudents.Take(TopStudentsCount).ToList();
             foreach (var x in topStudents)
             {
                 Console.WriteLine(x);
