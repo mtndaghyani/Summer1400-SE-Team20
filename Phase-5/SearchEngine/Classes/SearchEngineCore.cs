9a9c1a96711b557d4f2ef88a461b619afe690f28
mtndaghyani <matin.daghyani@gmail.com>
2021-08-09 17:56:12 +0430

    Add curly braces

--- Phase-5/SearchEngine/Classes/SearchEngineCore.cs
+++ Phase-5/SearchEngine/Classes/SearchEngineCore.cs
@@ -9,19 +9,33 @@ namespace SearchEngine.Classes
     {
         private const string SearchWordRegex = "([,.;'\"?!@#$%^&:*]*)([+-]?\\w+)([,.;'\"?!@#$%^&:*]*)";
         private readonly IInvertedIndex _invertedIndex;
-        public SearchEngineCore(IInvertedIndex invertedIndex) {
+        private static readonly Regex SearchRegex = new Regex(SearchWordRegex, RegexOptions.Compiled);
+
+        private static MatchCollection GetMatches(string statement)
+        {
+            return SearchRegex.Matches(statement);
+            ;
+        }
+
+        private static string GetSecondGroupMatched(Match match)
+        {
+            var groups = match.Groups;
+            var word = groups[2].Value;
+            return word;
+        }
+
+        public SearchEngineCore(IInvertedIndex invertedIndex)
+        {
             this._invertedIndex = invertedIndex;
         }
+
         public HashSet<int> search(string statement)
         {
-            SearchFields fields = new SearchFields();
-
-            Regex rx = new Regex(SearchWordRegex,
-                RegexOptions.Compiled);
-            MatchCollection matches = rx.Matches(statement);
-            foreach (Match match in matches) {
-                GroupCollection groups = match.Groups;
-                string word = groups[2].Value;
+            var fields = new SearchFields();
+            var matches = GetMatches(statement);
+            foreach (Match match in matches)
+            {
+                var word = GetSecondGroupMatched(match);
                 if (word.StartsWith("+"))
                 {
                     fields.AddPlusWord(_invertedIndex.Stem(word.Substring(1)));
@@ -35,10 +49,13 @@ namespace SearchEngine.Classes
                     fields.AddSimpleWord(_invertedIndex.Stem(word));
                 }
             }
+
             return AdvancedSearch(fields);
         }
-        
-        private HashSet<int> AdvancedSearch(SearchFields fields) {
+
+
+        private HashSet<int> AdvancedSearch(SearchFields fields)
+        {
             HashSet<int> result = new HashSet<int>();
             HandlePlusWords(fields, result);
             HandleSimpleWords(fields, result);
@@ -47,31 +64,41 @@ namespace SearchEngine.Classes
             return result;
         }
 
-        private void HandleMinusWords(ISearchFields fields, HashSet<int> result) {
-            foreach(string s in fields.GetMinusWords()){
+        private void HandleMinusWords(ISearchFields fields, HashSet<int> result)
+        {
+            foreach (string s in fields.GetMinusWords())
+            {
                 if (_invertedIndex.GetDictionary().ContainsKey(s))
                     result.ExceptWith(_invertedIndex.GetDictionary()[s]);
             }
         }
 
-        private void HandlePlusWords(ISearchFields fields, HashSet<int> result) {
-            if (!result.Any() && !fields.GetPlusWords().Any()){
-                foreach(string s in fields.GetSimpleWords())
+        private void HandlePlusWords(ISearchFields fields, HashSet<int> result)
+        {
+            if (!result.Any() && !fields.GetPlusWords().Any())
+            {
+                foreach (string s in fields.GetSimpleWords())
                     result.UnionWith(_invertedIndex.GetDictionary()[s]);
                 return;
             }
-            foreach(string s in fields.GetPlusWords()){
+
+            foreach (string s in fields.GetPlusWords())
+            {
                 if (_invertedIndex.GetDictionary().ContainsKey(s))
                     result.UnionWith(_invertedIndex.GetDictionary()[s]);
             }
         }
 
-        private void HandleSimpleWords(ISearchFields fields, HashSet<int> result) {
-            foreach(string s in fields.GetSimpleWords()){
-                if (!_invertedIndex.GetDictionary().ContainsKey(s)) {
+        private void HandleSimpleWords(ISearchFields fields, HashSet<int> result)
+        {
+            foreach (string s in fields.GetSimpleWords())
+            {
+                if (!_invertedIndex.GetDictionary().ContainsKey(s))
+                {
                     result.Clear();
                     return;
                 }
+
                 result.IntersectWith(_invertedIndex.GetDictionary()[s]);
             }
         }
