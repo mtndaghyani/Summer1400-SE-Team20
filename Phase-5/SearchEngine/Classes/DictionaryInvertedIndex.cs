25191844b75c28c297941067376fd2e76093721c
mtndaghyani <matin.daghyani@gmail.com>
2021-08-10 12:28:49 +0430

    Implement DictionaryInvertedIndex

---
+++ Phase-5/SearchEngine/Classes/DictionaryInvertedIndex.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using SearchEngine.Interfaces;
+
+namespace SearchEngine.Classes
+{
+    public class DictionaryInvertedIndex : IInvertedIndex<string, int>
+    {
+        private readonly Dictionary<string, HashSet<int>> _invertedIndex = new();
+
+        public bool ContainsKey(string key)
+        {
+            return _invertedIndex.ContainsKey(key);
+        }
+
+        public HashSet<int> Get(string key)
+        {
+            return _invertedIndex[key];
+        }
+
+        public void Add(string key, int value)
+        {
+            if (_invertedIndex.ContainsKey(key))
+            {
+                _invertedIndex[key].Add(value);
+            }
+            else
+            {
+                _invertedIndex[key] = new HashSet<int>() {value};
+            }
+        }
+    }
+}
