a0687c8dcec4ab3472810b1cedc8e4a1ce46334c
mtndaghyani <matin.daghyani@gmail.com>
2021-08-16 17:59:55 +0430

    Implement Console App

---
+++ Phase-5/SearchConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using SearchEngine.Classes;
+
+namespace SearchConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var manager = new Manager("../../../testConfig.json");
+            manager.Run();
+        }
+    }
+}
