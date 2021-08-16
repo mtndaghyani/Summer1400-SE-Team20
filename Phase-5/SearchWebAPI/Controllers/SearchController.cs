68890c376ed7b3a42dd3df174556facca72375b1
mtndaghyani <matin.daghyani@gmail.com>
2021-08-16 17:57:10 +0430

    Implement web api

---
+++ Phase-5/SearchWebAPI/Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SearchEngine.Classes.IO.Database.Models;
+using SearchEngine.Interfaces.Core;
+
+namespace SearchWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class SearchController : Controller
+    {
+        private readonly ISearchEngineCore _searchEngineCore;
+
+        public SearchController(ISearchEngineCore searchEngineCore)
+        {
+            _searchEngineCore = searchEngineCore;
+        }
+
+        [HttpGet()]
+        public List<Document> Query(string toSearch)
+        {
+            return new(_searchEngineCore.Search(toSearch));
+        }
+    }
+}
