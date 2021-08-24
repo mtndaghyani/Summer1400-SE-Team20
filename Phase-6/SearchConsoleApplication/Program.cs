256a3ca6ad7ca381d8996dfd39719a57873f2c22
mtndaghyani <matin.daghyani@gmail.com>
2021-08-24 13:17:56 +0430

    Add sample console app and test released package

---
+++ Phase-6/SearchConsoleApplication/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using SearchEngine.Classes;
+
+namespace SearchConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var manager = new Manager("../../../Config.json");
+            manager.Run();
+        }
+    }
+}
